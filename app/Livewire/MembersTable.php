3ff5bc7a61169af47498b8a3a4a7495903f3fbda
soybean15 <marlonpadilla1593@gmail.com>
2025-02-22 09:57:09 +0800

    Fixed null error on members

--- app/Livewire/MembersTable.php
+++ app/Livewire/MembersTable.php
@@ -39,8 +39,11 @@ final class MembersTable extends PowerGridComponent
     {
         return User::query()
             ->with(['membership', 'profile', 'profile.occupation'])
-            ->whereHas('membership'); // Ensures only users with a membership are included
+            ->whereHas('membership')
+            ->whereHas('profile');
+          
     }
+    
     
 
     public function relationSearch(): array
