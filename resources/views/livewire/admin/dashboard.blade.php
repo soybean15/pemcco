8dfa290392266f67456b44e40295a4bded487ea3
soybean15 <marlonpadilla1593@gmail.com>
2025-02-23 20:36:01 +0800

    Fixed dashboard

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,10 +1,10 @@
 <div class="p-6 ">
     <x-header title="Welcome Admin" subtitle="Fill in the required information to register a new member" info/>
 
-    <x-button wire:click='fixDb' label='Fix query' >
+    <x-button wire:click='fixDb' label='Fix query' />
 
     <!-- Stats Section -->
-<div class="grid grid-cols-1 gap-6 mt-4 md:grid-cols-4">
+    <div class="grid grid-cols-1 gap-6 mt-4 md:grid-cols-4">
         <!-- Active Members -->
         <x-stat :value="$this->activeCount +$this->inactiveCount+$this->terminatedCount" :title="'Total Members'" :subtitle="'Total Members'" :color="'primary'"/>
 
